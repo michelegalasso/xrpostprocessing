591c35fa23451456f78656b39e439543a4847fc5
Michele Galasso <m.galasso@yandex.com>
2019-12-14 17:57:00 +0300

    removed spaces from IUPAC formulas

--- xrpostproc/common/create_cif.py
+++ xrpostproc/common/create_cif.py
@@ -11,6 +11,10 @@ import spglib
 def create_cif(i, structure, ID, enthalpy, fitness, pmg_composition, pressure):
     dtset = spglib.get_symmetry_dataset((structure.lattice.matrix, structure.frac_coords, structure.atomic_numbers),
                                         symprec=0.2)
-    filename = '{}_EA{}_{:f}_{}_{}_{}_spg{}.cif'.format(i + 1, ID, fitness, enthalpy, pmg_composition.iupac_formula,
-                                                      pressure, dtset['number'])
+
+    # remove spaces from IUPAC formula
+    iupac_formula = pmg_composition.iupac_formula.replace(' ', '')
+
+    filename = '{}_EA{}_{:f}_{}_{}_{}_spg{}.cif'.format(i + 1, ID, fitness, enthalpy, iupac_formula, pressure,
+                                                        dtset['number'])
     structure.to(filename=os.path.join('results', filename), symprec=0.2)
