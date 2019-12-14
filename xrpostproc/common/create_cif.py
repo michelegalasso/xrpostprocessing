49d60f7d9f9a77397e2aa020ed83b03b8313a960
Michele Galasso <m.galasso@yandex.com>
2019-12-14 15:41:51 +0300

    added {:f} to fitness string format in order to limit the number of printed digits

--- xrpostproc/common/create_cif.py
+++ xrpostproc/common/create_cif.py
@@ -11,6 +11,6 @@ import spglib
 def create_cif(i, structure, ID, enthalpy, fitness, pmg_composition, pressure):
     dtset = spglib.get_symmetry_dataset((structure.lattice.matrix, structure.frac_coords, structure.atomic_numbers),
                                         symprec=0.2)
-    filename = '{}_EA{}_{}_{}_{}_{}_spg{}.cif'.format(i + 1, ID, fitness, enthalpy, pmg_composition.iupac_formula,
+    filename = '{}_EA{}_{:f}_{}_{}_{}_spg{}.cif'.format(i + 1, ID, fitness, enthalpy, pmg_composition.iupac_formula,
                                                       pressure, dtset['number'])
     structure.to(filename=os.path.join('results', filename), symprec=0.2)
