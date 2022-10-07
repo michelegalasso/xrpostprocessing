8c5c57cafdfac5b3b70404f8f9f32a2710e29f81
Michele Galasso <m.galasso@yandex.com>
2022-10-07 15:38:56 +0200

    now the auxiliary function read_structures works also for pythonic USPEX

--- xrpostproc/common/read_structures.py
+++ xrpostproc/common/read_structures.py
@@ -11,41 +11,79 @@ from .iterator_poscar_file import iterator_poscar_file
 
 def read_structures(individuals, gatheredPOSCARS, remove_hydrogens=False, fixcomp=False):
     iterator = iterator_poscar_file(gatheredPOSCARS)
+    uspexpy = False                 # suppose that we are reading MATLAB USPEX
     data = []
     with open(individuals, 'r') as f:
         while True:
             current_line = f.readline()
             if current_line == '':
                 break
-            values = current_line.split()
-            if values[0].isdigit():
-                composition_string = current_line[current_line.find('[') + 1:current_line.find(']')]
-                composition = [int(x) for x in composition_string.split()]
+            if '+----' in current_line:
+                uspexpy = True      # we are reading pythonic USPEX
+                continue
 
-                string = next(iterator)
-                ID = string.split()[0].strip('EA')
-
-                if fixcomp:
-                    ID_position = 1
+            if uspexpy:
+                values = current_line.split('|')
+                if 'ID' in values[1]:
+                    headers = []
+                    for value in values:
+                        headers.append(value.strip())
                 else:
-                    ID_position = 0
+                    string = next(iterator)
+                    ID = string.split()[0].strip('EA')
 
-                if ID == values[ID_position]:
-                    structure = Structure.from_str(string, fmt='poscar')
-                    pmg_composition = structure.composition
+                    if ID == values[1].strip():
+                        structure = Structure.from_str(string, fmt='poscar')
+                        pmg_composition = structure.composition
 
-                    if remove_hydrogens:
-                        structure.remove_species(['H'])
+                        if remove_hydrogens:
+                            structure.remove_species(['H'])
 
-                    if len(structure.frac_coords) != 0:
-                        if fixcomp:
-                            enthalpy = float(values[5 + len(composition)])
-                            fitness = float(values[5 + len(composition)]) / structure.num_sites
-                        else:
-                            enthalpy = float(values[3 + len(composition)])
-                            fitness = float(values[5 + len(composition)])
+                        if len(structure.frac_coords) != 0:
+                            enthalpy_position = headers.index('Enthalpy (eV)')
 
-                        data.append((structure, ID, enthalpy, fitness, pmg_composition))
-                else:
-                    raise IOError('Structures in {} do not match data in {}'.format(gatheredPOSCARS, individuals))
+                            if fixcomp:
+                                enthalpy = float(values[enthalpy_position])
+                                fitness = float(values[enthalpy_position]) / structure.num_sites
+                            else:
+                                fitness_position = headers.index('Enthalpy per Block above CCH (eV)')
+
+                                enthalpy = float(values[enthalpy_position])
+                                fitness = float(values[fitness_position])
+
+                            data.append((structure, ID, enthalpy, fitness, pmg_composition))
+                    else:
+                        raise IOError('Structures in {} do not match data in {}'.format(gatheredPOSCARS, individuals))
+            else:
+                values = current_line.split()
+                if values[0].isdigit():
+                    composition_string = current_line[current_line.find('[') + 1:current_line.find(']')]
+                    composition = [int(x) for x in composition_string.split()]
+
+                    string = next(iterator)
+                    ID = string.split()[0].strip('EA')
+
+                    if fixcomp:
+                        ID_position = 1
+                    else:
+                        ID_position = 0
+
+                    if ID == values[ID_position]:
+                        structure = Structure.from_str(string, fmt='poscar')
+                        pmg_composition = structure.composition
+
+                        if remove_hydrogens:
+                            structure.remove_species(['H'])
+
+                        if len(structure.frac_coords) != 0:
+                            if fixcomp:
+                                enthalpy = float(values[5 + len(composition)])
+                                fitness = float(values[5 + len(composition)]) / structure.num_sites
+                            else:
+                                enthalpy = float(values[3 + len(composition)])
+                                fitness = float(values[5 + len(composition)])
+
+                            data.append((structure, ID, enthalpy, fitness, pmg_composition))
+                    else:
+                        raise IOError('Structures in {} do not match data in {}'.format(gatheredPOSCARS, individuals))
     return data
