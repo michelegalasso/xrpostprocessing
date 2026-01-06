b6f28ca6e3246263d68ca886239f200808828aa4
Michele Galasso <galasmic@cvut.cz>
2026-01-06 11:29:49 +0100

    Updated the description of the powder_xrd_screening.py script in the documentation

--- xrpostproc/powder_xrd_screening/powder_xrd_screening.py
+++ xrpostproc/powder_xrd_screening/powder_xrd_screening.py
@@ -12,15 +12,15 @@ from xrpostproc.common.SpectrumAnalyzer import SpectrumAnalyzer
 
 
 # input parameters (BaH)
-th_peaks_penalty = 5.0                                          # additional penalty for non-matching th_peaks
-k_coeffs = np.arange(0.91, 1.09, 0.005)                         # coefficients for isotropic cell deformation
 gathered_POSCARS = 'UH_50_extended_convex_hull_POSCARS.txt'     # file with all structures in the POSCAR format
 spectrum_file = 'UH_50_powder.txt'                              # name of the experimental spectrum file
 spectrum_starts = 6.0                                           # minimum angle for theoretical spectra
 spectrum_ends = 22.0                                            # maximum angle for theoretical spectra
-wavelength = 0.4130                                             # experimental wavelength
+k_coeffs = np.arange(0.91, 1.09, 0.005)                         # coefficients for isotropic cell deformation
 sigma = 0.01                                                    # parameter for gaussian smearing of peaks
+wavelength = 0.4130                                             # experimental wavelength
 match_tol = 0.25                                                # tolerance for matching peaks in degrees
+th_peaks_penalty = 5.0                                          # additional penalty for non-matching th_peaks
 
 analyzer = SpectrumAnalyzer(gathered_POSCARS=gathered_POSCARS, spectrum_file=spectrum_file,
                             spectrum_starts=spectrum_starts, spectrum_ends=spectrum_ends, wavelength=wavelength,
